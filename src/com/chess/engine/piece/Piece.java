d2d98a7460209f224c8123cc999d9e9a5404fb44
ajakupi <arjan.jakupi007@gmail.com>
2022-08-31 14:47:05 +0200

    Board class using Builder philosophy

--- src/com/chess/engine/piece/Piece.java
+++ src/com/chess/engine/piece/Piece.java
@@ -20,6 +20,9 @@ public abstract class Piece {
         this.isFirstMove = false;
     }
 
+    public int getPiecePosition(){
+        return this.piecePosition;
+    }
     public Alliance getPieceAlliance(){
         return this.pieceAlliance;
     }
