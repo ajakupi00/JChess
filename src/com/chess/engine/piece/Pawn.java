26c78a6741f5b5f23a21762fde7354c8317233e3
ajakupi <arjan.jakupi007@gmail.com>
2022-08-31 14:10:43 +0200

    Pawn - Exceptions moves

--- src/com/chess/engine/piece/Pawn.java
+++ src/com/chess/engine/piece/Pawn.java
@@ -5,13 +5,14 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Move.MajorMove;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Pawn extends Piece{
-    private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {7, 8, 9, 16};
 
     Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -39,9 +40,28 @@ public class Pawn extends Piece{
                         !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
                     legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
                 }
+            }else if(currentCandidateOffset == 7 &&
+                    !((BoardUtils.EIGHTH_COLUMN[this.piecePosition] &&  this.pieceAlliance.isWhite() ||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())))){
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance()){
+                        //TODO more to do here
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            }else if(currentCandidateOffset == 9 &&
+                    !((BoardUtils.FIRST_COLUMN[this.piecePosition] &&  this.pieceAlliance.isWhite() ||
+                     (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())))){
+
+                final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                if(this.pieceAlliance != pieceOnCandidate.getPieceAlliance()){
+                    //TODO more to do here
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
             }
         }
 
-        return legalMoves;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
