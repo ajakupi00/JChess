b697a175d6dfe6f6d0b71d13df2a69e6f093c3b9
ajakupi <arjan.jakupi007@gmail.com>
2022-09-02 00:30:34 +0200

    Board execute method general idea written

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,14 +1,15 @@
 package com.chess.engine.board;
 
+import com.chess.engine.board.Board.Builder;
 import com.chess.engine.piece.Piece;
 
 public abstract class Move {
     final Board board;
-    final Piece piece;
+    final Piece movedPiece;
     final int destinationCoordinate;
-    private Move(Board board, Piece piece, int destinationCoordinate){
+    private Move(Board board, Piece movedPiece, int destinationCoordinate){
         this.board = board;
-        this.piece = piece;
+        this.movedPiece = movedPiece;
         this.destinationCoordinate = destinationCoordinate;
     }
 
@@ -20,20 +21,34 @@ public abstract class Move {
 
     public static final class MajorMove extends Move{
 
-        public MajorMove(final Board board,final Piece piece,final int destinationCoordinate) {
-            super(board, piece, destinationCoordinate);
+        public MajorMove(final Board board,final Piece movedPiece,final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
         }
 
         @Override
         public Board execute() {
-            return null;
+            final Builder builder = new Builder();
+            for(final Piece piece : this.board.currentPlayer().getActivePieces()){
+                //TODO hashcode and equals for pieces
+                if(!this.movedPiece.equals(piece)){
+                    builder.setPiece(piece);
+                }
+            }
+
+            for(final Piece piece : this.board.currentPlayer().getOpponent().getActivePieces()){
+                builder.setPiece(piece);
+            }
+
+            builder.setPiece(null);
+            builder.setMoveMaker(this.board.currentPlayer().getOpponent().getAlliance());
+            return builder.build();
         }
     }
 
     public static final class AttackMove extends Move{
         final Piece attackedPiece;
-        public AttackMove(final Board board,final Piece piece,final int destinationCoordinate, final Piece attackedPiece) {
-            super(board, piece, destinationCoordinate);
+        public AttackMove(final Board board,final Piece movedPiece,final int destinationCoordinate, final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoordinate);
             this.attackedPiece = attackedPiece;
         }
 
